c6846a139d32acc3f11a872514228d33aa850321
AB-code-crypto <alex-god@mail.ru>
2026-04-18 16:14:10 +0300

    делаю первый запрос к ПЛ.

--- app_providers/models/provider.py
+++ app_providers/models/provider.py
@@ -77,7 +77,7 @@ class ProviderCode(models.TextChoices):
 
 
 PROVIDER_DEFAULTS = {
-    ProviderCode.WHITEBIT: {"provider_type": ProviderType.CEX, "affiliate_url": "https://whitebit.com/ref/your-ref-code"},
+    ProviderCode.WHITEBIT: {"provider_type": ProviderType.CEX, "affiliate_url": "https://whitebit.com/ru"},
     ProviderCode.MEXC: {"provider_type": ProviderType.CEX, "affiliate_url": ""},
     # ProviderCode.BYBIT: {"provider_type": ProviderType.CEX, "affiliate_url": ""},
     # ProviderCode.BINANCE: {"provider_type": ProviderType.CEX, "affiliate_url": ""},
@@ -85,28 +85,18 @@ PROVIDER_DEFAULTS = {
 
 
 class Provider(UUIDTimestampedModel):
-    code = models.CharField(max_length=64, choices=ProviderCode.choices, unique=True, db_index=True, verbose_name="Провайдер",
-                            help_text="Выберите одного из поддерживаемых провайдеров.")
+    code = models.CharField(max_length=64, choices=ProviderCode.choices, unique=True, db_index=True, verbose_name="Провайдер",help_text="Выберите одного из поддерживаемых провайдеров.")
     provider_type = models.CharField(max_length=32, choices=ProviderType.choices, editable=False, db_index=True, verbose_name="Тип провайдера")
-    priority = models.PositiveIntegerField(default=100, validators=[MinValueValidator(1)], db_index=True, verbose_name="Приоритет",
-                                           help_text="Чем меньше число, тем выше приоритет.")
-    affiliate_url = models.URLField(blank=True, verbose_name="Партнёрская ссылка",
-                                    help_text="Подставляется автоматически, но при необходимости можно изменить вручную.")
+    priority = models.PositiveIntegerField(default=100, validators=[MinValueValidator(1)], db_index=True, verbose_name="Приоритет",help_text="Чем меньше число, тем выше приоритет.")
+    affiliate_url = models.URLField(blank=True, verbose_name="Партнёрская ссылка",help_text="Подставляется автоматически, но при необходимости можно изменить вручную.")
     description = models.TextField(blank=True, verbose_name="Описание")
-    price_feed_enabled = models.BooleanField(default=False, verbose_name="Трансляция цен",
-                                             help_text="Провайдер может использоваться как источник цен. Это относится к биржам, обменникам, агрегаторам, ЦБ РФ и другим источникам котировок. Если провайдер нужен только для движения средств без получения цен, это поле можно выключить.")
-    deposit_enabled = models.BooleanField(default=False, verbose_name="Ввод средств",
-                                          help_text="Через этого провайдера можно принимать средства. Например: биржа, обменник, кошелёк, нода или другой платёжный/расчётный контур, на который можно завести актив.")
-    address_generation_enabled = models.BooleanField(default=False, verbose_name="Генерация адресов",
-                                                     help_text="Можно ли автоматически получать адрес для пополнения через этого провайдера. Это поле относится именно к генерации или выдаче депозитного адреса, а не просто к факту, что провайдер умеет принимать средства.")
-    withdraw_enabled = models.BooleanField(default=False, verbose_name="Вывод средств",
-                                           help_text="Через этого провайдера можно отправлять средства наружу. Например: вывод с биржи, отправка с кошелька или перевод через собственную ноду.")
-    otc_enabled = models.BooleanField(default=False, verbose_name="ОТС обмен",
-                                      help_text="Провайдер поддерживает обмен, но это не спот. Используется для ОТС рынков При ОТС обмене не применяется комиссия провайдера")
-    spot_trading_enabled = models.BooleanField(default=False, verbose_name="Спот торговля",
-                                               help_text="Провайдер поддерживает спотовую торговлю или спотовый обмен. Эта функция разрешает обмен активов на стороне провайдера")
-    futures_trading_enabled = models.BooleanField(default=False, verbose_name="Фьючерсы",
-                                                  help_text="Провайдер поддерживает торговлю фьючерсами. Используется для хеджирования средств")
+    price_feed_enabled = models.BooleanField(default=False, verbose_name="Трансляция цен",help_text="Провайдер может использоваться как источник цен. Это относится к биржам, обменникам, агрегаторам, ЦБ РФ и другим источникам котировок. Если провайдер нужен только для движения средств без получения цен, это поле можно выключить.")
+    deposit_enabled = models.BooleanField(default=False, verbose_name="Ввод средств",help_text="Через этого провайдера можно принимать средства. Например: биржа, обменник, кошелёк, нода или другой платёжный/расчётный контур, на который можно завести актив.")
+    address_generation_enabled = models.BooleanField(default=False, verbose_name="Генерация адресов",help_text="Можно ли автоматически получать адрес для пополнения через этого провайдера. Это поле относится именно к генерации или выдаче депозитного адреса, а не просто к факту, что провайдер умеет принимать средства.")
+    withdraw_enabled = models.BooleanField(default=False, verbose_name="Вывод средств",help_text="Через этого провайдера можно отправлять средства наружу. Например: вывод с биржи, отправка с кошелька или перевод через собственную ноду.")
+    otc_enabled = models.BooleanField(default=False, verbose_name="ОТС обмен",help_text="Провайдер поддерживает обмен, но это не спот. Используется для ОТС рынков При ОТС обмене не применяется комиссия провайдера")
+    spot_trading_enabled = models.BooleanField(default=False, verbose_name="Спот торговля",help_text="Провайдер поддерживает спотовую торговлю или спотовый обмен. Эта функция разрешает обмен активов на стороне провайдера")
+    futures_trading_enabled = models.BooleanField(default=False, verbose_name="Фьючерсы",help_text="Провайдер поддерживает торговлю фьючерсами. Используется для хеджирования средств")
 
     class Meta:
         verbose_name = "Провайдер"
