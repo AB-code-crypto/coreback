8b08108c5b073783aabb3a37566c411366f90379
AB-code-crypto <alex-god@mail.ru>
2026-04-19 23:19:34 +0300

    добавляем MEXC

--- app_providers/admin/provider_stats.py
+++ app_providers/admin/provider_stats.py
@@ -121,7 +121,7 @@ class ProviderStatsAdmin(admin.ModelAdmin):
         (
             "Детальная статистика",
             {
-                "description": "Обменная пара: BASE/QUOTE",
+                "description": "Обменная пара: BASE / QUOTE",
                 "fields": (
                     "quote_asset_counts_display",
                     "stablecoin_pair_counts_display",
