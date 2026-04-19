4db882ebdd2f60645d88363e8f220c77b4a535ef
AB-code-crypto <alex-god@mail.ru>
2026-04-19 16:21:19 +0300

    добавляем MEXC

--- app_providers/admin/__init__.py
+++ app_providers/admin/__init__.py
@@ -1,7 +1,7 @@
-from .provider import ProviderAdmin
-from .provider_api import ProviderApiAdmin
-from .provider_stats import ProviderStatsAdmin
-from .provider_asset_context import ProviderAssetContextAdmin
+from app_providers.admin.provider import ProviderAdmin
+from app_providers.admin.provider_api import ProviderApiAdmin
+from app_providers.admin.provider_stats import ProviderStatsAdmin
+from app_providers.admin.provider_asset_context import ProviderAssetContextAdmin
 
 __all__ = [
     "ProviderAdmin",
